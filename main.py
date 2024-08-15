d7be5e72f44fa57da2e428cc90c2803bd95b84d1
nizarsaidicoder <nizar.saidi.coder@gmail.com>
2024-08-15 12:42:20 +0200

    Scraped characters and light cones

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Start scraping ...
+import requests
+from bs4 import BeautifulSoup
+import csv
+import json
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+BASE_URL = "https://www.prydwen.gg/page-data/star-rail/"
+CHARACTERS_URL = BASE_URL + "characters/page-data.json"
+LIGHTCONES_URL = BASE_URL + "light-cones/page-data.json"
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def get_character_info(character_name):
+    response = requests.get(BASE_URL + "characters/" + character_name + "/page-data.json")
+    data = response.json()
+    character = data["result"]["data"]["currentUnit"]["nodes"][0]
+    # print(character)
+    #      store in ./characters/character_name.json
+    with open("./characters/" + character_name + ".json", "w") as file:
+        # formatted json output
+        json.dump(character, file, indent=4)
+
+
+def get_characters_name():
+    response = requests.get(CHARACTERS_URL)
+    data = response.json()
+    characters = data["result"]["data"]["allCharacters"]["nodes"]
+    for character in characters:
+        get_character_info(character["slug"])
+
+
+def get_lightcones():
+    response = requests.get(LIGHTCONES_URL)
+    data = response.json()
+    lightcones = data["result"]["data"]["allCharacters"]["nodes"]
+    for lightcone in lightcones:
+        get_lightcone_info(lightcone)
+
+def get_lightcone_info(lightcone):
+    with open("./light-cones/" + lightcone["slug"] + ".json", "w") as file:
+        json.dump(lightcone, file, indent=4)
+
+get_lightcones()
